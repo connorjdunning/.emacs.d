0165ed718aa80af0c7548021eb7179cdc82d3b27
connorjdunning <connorjdunning@gmail.com>
2025-02-11 20:36:35 -0800

    magit, early-init in config.org

--- early-init.el
+++ early-init.el
@@ -1,3 +1,5 @@
 ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Early-Init-File.html
-(setq package-enable-at-startup nil)
-(setenv "LSP_USE_PLISTS" "true")
+(when (string-equal (file-name-nondirectory (or load-file-name buffer-file-name)) "early-init.el")
+  (message "Loading early-init.el")
+  (setq package-enable-at-startup nil)
+  (setenv "LSP_USE_PLISTS" "true"))
