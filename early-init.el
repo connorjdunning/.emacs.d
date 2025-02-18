5fb232f9885a0653b550d123ee26fa200533eca9
connorjdunning <connorjdunning@gmail.com>
2025-02-17 18:39:37 -0800

    Adding repo, manual, and docs links; minor Formatting

--- early-init.el
+++ early-init.el
@@ -1,4 +1,3 @@
-;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Early-Init-File.html
 (when (string-equal (file-name-nondirectory (or load-file-name buffer-file-name)) "early-init.el")
   (message "Loading early-init.el")
   (setq package-enable-at-startup nil)
